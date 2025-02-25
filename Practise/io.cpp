b5932d32f8e6ef0c542ba8e437a4a2ae67ac7c6b
divyanshukumar10 <divyanshu.kr10@gmail.com>
2025-02-25 23:32:14 +0530

    Practise

---
+++ Practise/io.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int m,n;
+    cout<<"Please enter your numbers:"<<endl;
+    cin>>m;
+    cin>>n;
+    cout<<"sum: "<<m + n<<endl;
+    return 0;
+}
