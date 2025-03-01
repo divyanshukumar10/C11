6cec9634f860de11ceb7c4a4fbefad28f190a624
divyanshukumar10 <divyanshu.kr10@gmail.com>
2025-03-01 21:04:13 +0530

    code added

---
+++ Practise/area.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int length,breadth;
+    cin>>length;
+    cin>>breadth;
+    cout<<"area = "<<length*breadth<<"sqft"<<endl;
+    return 0;
+}
