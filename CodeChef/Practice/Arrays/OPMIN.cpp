2cfcb0bb1ec0e58ae282359c6c757841b0351fc8
divyanshukumar10 <divyanshu.kr10@gmail.com>
2025-03-04 00:31:17 +0530

    CC4

---
+++ CodeChef/Practice/Arrays/OPMIN.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        
+        int arr[n];
+        
+        for(int i=0;i<n;i++)
+        {
+            cin>>arr[i];
+        }
+        
+        int count=0;
+        int m = arr[0];
+        
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]<=m)
+            {
+                m=arr[i];
+            }
+        }
+        
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]>m)
+            {
+                count++;
+            }
+        }
+        
+        cout<<count<<endl;
+        
+    }
+    
+    
+}
